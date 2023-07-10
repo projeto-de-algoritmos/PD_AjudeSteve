68180458aefe74118cd731073aa9525a5b97cba1
Weslley Barros <weslley17w@gmail.com>
2023-07-10 13:15:44 -0300

    Adicina publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/PD_AjudeSteve/" : "/",
   transpileDependencies: true,
 });
